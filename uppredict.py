0ba96d4d7d64307c68198ad90b0203b4c4ec25f0
SultanMtm <149297979+SultanMtm@users.noreply.github.com>
2025-06-30 13:51:54 +0700

    Update uppredict.py

--- uppredict.py
+++ uppredict.py
@@ -7,7 +7,7 @@ import numpy as np
 from PIL import Image
 
 # Load model
-model = load_model("alatmusiktrad.h5", compile=False)
+model = load_model("alatmusiktrad.h5")
 
 # Kelas alat musik tradisional dan deskripsinya
 class_info = {
